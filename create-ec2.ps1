ed1668122d408b3c7a53d29a384dc6c8770a556c
Kulbhushan <kulbhushan.mayer@>
2020-12-26 11:27:49 +0530

    parsed key-pair to used

--- create-ec2.ps1
+++ create-ec2.ps1
@@ -1,2 +1,9 @@
 ﻿$imageDetails = aws ec2 describe-images --filters Name="tag:Name",Values="thinknyx-httpd-java" | ConvertFrom-Json
-$imageDetails.Images.ImageId
+$amiID = $imageDetails.Images.ImageId
+
+$keyPairs = aws ec2 describe-key-pairs| ConvertFrom-Json
+foreach($keyPair in $keyPairs.KeyPairs){
+    if ($keyPair.KeyName -match "ericsson"){
+        $keyName = $keyPair.KeyName
+    }
+}
