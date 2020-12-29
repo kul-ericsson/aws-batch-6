2b7195ddb1799c6e6506cc1c360c57987ef05a2f
Kulbhushan <kulbhushan.mayer@>
2020-12-29 15:59:13 +0530

    fetching details about private subnets

--- terraform/create_compute/data_sources.tf
+++ terraform/create_compute/data_sources.tf
@@ -8,6 +8,14 @@ data "aws_subnet" "public" {
   vpc_id = data.aws_vpc.vpc.id
   availability_zone = "us-east-2a"
 }
+data "aws_subnet" "private_2" {
+  vpc_id = data.aws_vpc.vpc.id
+  availability_zone = "us-east-2b"
+}
+data "aws_subnet" "private_3" {
+  vpc_id = data.aws_vpc.vpc.id
+  availability_zone = "us-east-2c"
+}
 data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.vpc.id
   filter {
