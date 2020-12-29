8bfa2a9cb28679baa2c6936964e1fe80dfc54fb7
Kulbhushan <kulbhushan.mayer@>
2020-12-29 16:52:37 +0530

    trigger sql script using null resource alsong with depends_on feature

--- terraform/create_compute/null_resource_with_provisioners.tf
+++ terraform/create_compute/null_resource_with_provisioners.tf
@@ -25,3 +25,18 @@ resource "null_resource" "copy_demo_sql_script" {
     destination = "/tmp/demo_sql_script.sql"
   }
 }
+
+resource "null_resource" "run_demo_sql_script" {
+  depends_on = [ null_resource.copy_demo_sql_script, null_resource.install_mysql_client ]
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("C:/training/ericsson/aws/kul-ericsson-thinknyx.pem")
+      host = aws_instance.server_public.public_ip
+    }
+    inline = [
+      "mysql -h ${aws_db_instance.thinknyx_db.address} -u ${aws_db_instance.thinknyx_db.username} -padmin123 -D ${mysql_database.thinknyx_database.name} </tmp/demo_sql_script.sql"
+    ]
+  }
+}
