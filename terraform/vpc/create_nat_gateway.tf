bb6dee686dc34c69cbcc981cbc8e79502db52da0
Kulbhushan <kulbhushan.mayer@>
2020-12-29 11:06:49 +0530

    create NAT Gateway

--- terraform/vpc/create_nat_gateway.tf
+++ terraform/vpc/create_nat_gateway.tf
@@ -3,3 +3,10 @@ resource "aws_eip" "thinknyx_eip" {
     "Name" = var.Name
   }
 }
+resource "aws_nat_gateway" "thinknyx_ngw" {
+  allocation_id = aws_eip.thinknyx_eip.id
+  subnet_id = aws_subnet.thinknyx_subnet_1.id
+  tags = {
+    "Name" = var.Name
+  }
+}
