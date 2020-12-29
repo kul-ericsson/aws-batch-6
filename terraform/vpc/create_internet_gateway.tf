4a3df3de78bccaca30c48a5e94c1c2364afe00c8
Kulbhushan <kulbhushan.mayer@>
2020-12-29 10:59:15 +0530

    create public subnet & Internet Gateway Route Table Association

--- terraform/vpc/create_internet_gateway.tf
+++ terraform/vpc/create_internet_gateway.tf
@@ -8,10 +8,15 @@ resource "aws_internet_gateway" "thinknyx_igw" {
 resource "aws_route_table" "thinknyx_rt_with_igw" {
   vpc_id = aws_vpc.thinknyx_vpc.id
   tags = {
-    "Name" = var.Name
+    "Name" = "${var.Name}_public_route"
   }
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.thinknyx_igw.id
   }
+}
+
+resource "aws_route_table_association" "thinknyx_rt_igw_association" {
+  subnet_id = aws_subnet.thinknyx_subnet_1.id
+  route_table_id = aws_route_table.thinknyx_rt_with_igw.id
 }
