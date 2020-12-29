3d3ccd5b01af36bedf14b95ef6da3d7368f256ee
Kulbhushan <kulbhushan.mayer@>
2020-12-29 15:24:38 +0530

    updated to use both newly created & default security group for public subnet server

--- terraform/create_compute/create_ec2_servers.tf
+++ terraform/create_compute/create_ec2_servers.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "server_public" {
   tags = {
     "Name" = "${var.Name}_${var.suffix}"
   }
+  vpc_security_group_ids = [ aws_security_group.thinknyx_sg.id, data.aws_security_group.vpc_default_sg.id ]
 }
 resource "aws_instance" "server_private" {
   for_each = data.aws_subnet_ids.private.ids
