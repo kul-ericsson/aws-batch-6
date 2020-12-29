af6e4d51d33d916e86f3fc9b3bc5c3c2b872afa3
Kulbhushan <kulbhushan.mayer@>
2020-12-29 16:18:40 +0530

    output private ips of server in private subnets

--- terraform/create_compute/outputs.tf
+++ terraform/create_compute/outputs.tf
@@ -3,4 +3,8 @@ output "public_server_ip"{
 }
 output "mysql_address"{
   value = aws_db_instance.thinknyx_db.address
-} 
+}
+output private_server_ips {
+  description = "IDs of EC2 instances"
+  value       = { for p in (data.aws_subnet_ids.private.ids) : p => aws_instance.server_private[p].private_ip }
+}
