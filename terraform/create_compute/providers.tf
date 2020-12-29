06c521218d6c911f6ffacb96644b4fbd283178ea
Kulbhushan <kulbhushan.mayer@>
2020-12-29 15:42:44 +0530

    added MYSQL provider with its source location

--- terraform/create_compute/providers.tf
+++ terraform/create_compute/providers.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-2"
 }
+provider "mysql" {
+  endpoint = ""
+  username = ""
+  password = ""
+}
+
+terraform {
+  required_providers {
+    mysql = {
+      source = "terraform-providers/mysql"
+    }
+  }
+}
