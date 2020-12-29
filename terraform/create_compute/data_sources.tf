e73b72edcbf57ee17404718b1620b133a036ab97
Kulbhushan <kulbhushan.mayer@>
2020-12-29 12:43:35 +0530

    fetch private subnet IDs

--- terraform/create_compute/data_sources.tf
+++ terraform/create_compute/data_sources.tf
@@ -8,3 +8,10 @@ data "aws_subnet" "public" {
   vpc_id = data.aws_vpc.vpc.id
   availability_zone = "us-east-2a"
 }
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.vpc.id
+  filter {
+    name = "tag:Name"
+    values = [var.Name]
+  }
+}
