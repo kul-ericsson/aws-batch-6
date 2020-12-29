4641cdbf5e9b20937dbeca35cf0ba965d24e5187
Kulbhushan <kulbhushan.mayer@>
2020-12-29 10:54:38 +0530

    create Route Table with Internet Gateway Route

--- terraform/vpc/create_internet_gateway.tf
+++ terraform/vpc/create_internet_gateway.tf
@@ -4,3 +4,14 @@ resource "aws_internet_gateway" "thinknyx_igw" {
     "Name" = var.Name
   }
 }
+
+resource "aws_route_table" "thinknyx_rt_with_igw" {
+  vpc_id = aws_vpc.thinknyx_vpc.id
+  tags = {
+    "Name" = var.Name
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.thinknyx_igw.id
+  }
+}
